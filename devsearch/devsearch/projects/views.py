38bb00c98c6a4b46fd458d2fb659eecc3c46052b
vytzab <vytautas.zabielskas@gmail.com>
2022-12-06 11:30:34 +0200

    Setting up templates structure

--- devsearch/devsearch/projects/views.py
+++ devsearch/devsearch/projects/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def projects(request):
-    return HttpResponse("Here are our products")
+    return render(request, "projects/projects.html")
 
 def project(request, pk):
-    return HttpResponse("SINGLE PROJECT" + " " + str(pk))
+    return render(request, "projects/single-project.html")
