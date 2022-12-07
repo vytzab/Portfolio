65f7c1fdca57f4ea1e4930ddc287781f96c2abc4
vytzab <vytautas.zabielskas@gmail.com>
2022-12-07 07:04:11 +0200

    Create Read Update Delete

--- devsearch/devsearch/projects/models.py
+++ devsearch/devsearch/projects/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 import uuid
 
+
+
 class Project(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField(null=True, blank=True)
