5f56be423613b66b6678e64019a8f6c5c8d6522f
vytzab <vytautas.zabielskas@gmail.com>
2022-12-08 22:31:00 +0200

    added registration and styling

--- devsearch/devsearch/users/views.py
+++ devsearch/devsearch/users/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from .models import Profile
+from .forms import CustomUserCreationForm
 
 
 def loginUser(request):
+    page = "login"
+    
     if request.user.is_authenticated:
         return redirect("profiles")
         
@@ -32,8 +36,30 @@ def loginUser(request):
 
 def logoutUser(request):
     logout(request)
-    messages.error(request, "User was logged out.")
+    messages.info(request, "User was logged out.")
     return redirect("login")
+
+def registerUser(request):
+    page = "register"
+    form = CustomUserCreationForm()
+
+    if request.method == "POST":
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+            
+            messages.success(request, "User account was created!")
+            
+            login(request, user)
+            return redirect("profiles")
+        
+        else:
+            messages.error(request, "An error has occurred during registration!")
+            
+    context = {"page" : page, "form" : form}
+    return render(request, "users/login_register.html", context)
 
 def profiles(request):
     profiles = Profile.objects.all()
